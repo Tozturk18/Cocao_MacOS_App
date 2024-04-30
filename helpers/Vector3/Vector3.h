73b3ec0438fcdd71e560254b47deec4cb688e119
tozturk18 <tunaozturk2001@hotmail.com>
2024-04-29 20:18:34 -0400

    Added vector3 object for future implementations

---
+++ helpers/Vector3/Vector3.h
@@ -0,0 +1,31 @@
+/**
+ * @file Vector3.h
+ * @author Ozgur Tuna Ozturk (tunaozturk2001@hotmail.com)
+ * @brief This is the header file for the Vector3 object. 
+ * This object is used to represent a 3D vector in the space.
+ * @version 0.1
+ * @date 2024-04-29
+ * 
+ * @copyright MIT (c) 2024
+ * 
+ */
+
+/* --- IMPORTS --- */
+#import <Foundation/Foundation.h>
+/* --- End of IMPORTS --- */
+
+/**
+ * @brief This is the Vector3 object. 
+ * This object is used to represent a 3D vector in the space.
+ * 
+ */
+@interface Vector3 : NSObject
+
+@property (nonatomic, assign) CGFloat x; // X coordinate
+@property (nonatomic, assign) CGFloat y; // Y coordinate
+@property (nonatomic, assign) CGFloat z; // Z coordinate
+
+- (instancetype)initWithX:(CGFloat)x y:(CGFloat)y z:(CGFloat)z;
++ (instancetype)vector3WithX:(CGFloat)x y:(CGFloat)y z:(CGFloat)z;
+
+@end
---
+++ helpers/Vector3/Vector3.m
@@ -0,0 +1,31 @@
+/**
+ * @file Vector3.m
+ * @author Ozgur Tuna Ozturk (tunaozturk2001@hotmail.com)
+ * @brief This is the implementation file of the Vector3 class.
+ * This class is used to represent 3D vectors. This class has 3 methods,
+ * first two of them are for initialization and the other one is a class method that
+ * creates an array of Vector3 objects from an array of NSValue objects.
+ * @version 0.1
+ * @date 2024-04-29
+ * 
+ * @copyright MIT (c) 2024
+ * 
+ */
+
+#import "Vector3.h"
+
+@implementation Vector3
+
+- (instancetype)initWithX:(CGFloat)x y:(CGFloat)y z:(CGFloat)z {
+    self = [super init];
+    if (self) {
+        _x = x;
+        _y = y;
+        _z = z;
+    }
+    return self;
+}
+
++ (instancetype)vector3WithX:(CGFloat)x y:(CGFloat)y z:(CGFloat)z {
+    return [[Vector3 alloc] initWithX:x y:y z:z];
+}
